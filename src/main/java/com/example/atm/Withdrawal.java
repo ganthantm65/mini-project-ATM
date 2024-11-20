cad84f693023715f08dcf598c2e57490b71edb2c
Ganthan T.M <144930744+ganthantm65@users.noreply.github.com>
2024-11-20 20:27:42 +0530

    Update Withdrawal.java

--- src/main/java/com/example/atm/Withdrawal.java
+++ src/main/java/com/example/atm/Withdrawal.java
@@ -7,6 +7,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.TextField;
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
 import javafx.stage.Stage;
 
 import java.io.IOException;
